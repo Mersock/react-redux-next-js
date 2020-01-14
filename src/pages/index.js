a269f07979f44dceaae3d6ff719681f0dcae917d
Phumthawan <knzphumthawan@gmail.com>
2020-01-14 15:07:34 +0700

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,24 @@ import { connect } from 'react-redux';
 import { getPosts } from '../redux/actions/fooActions';
 
 const Index = props => {
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.getPosts();
-    };
-    return (
-        <div>
-            <div>Prop from Redux {JSON.stringify(props)}</div>
-            <button onClick={handleSubmit}>Load</button>
-            <div>Prop from getInitialProps {props.custom}</div>
-        </div>
-    );
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.getPosts();
+  };
+  return (
+    <div>
+      <div>Prop from Redux</div>
+      <div>{JSON.stringify(props)}</div>
+      <button onClick={handleSubmit}>Load</button>
+      <br />
+      <div>Prop from getInitialProps {props.custom}</div>
+    </div>
+  );
 };
 
 Index.getInitialProps = async ({ store, isServer, pathname, query }) => {
-    await store.dispatch(getPosts());
-    return { custom: 'custom' };
+  await store.dispatch(getPosts());
+  return { custom: 'custom' };
 };
 
-export default connect(
-    state => state,
-    { getPosts }
-)(Index);
+export default connect(state => state, { getPosts })(Index);
